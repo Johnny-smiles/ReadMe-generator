a0d4b967e8b3cae4605fa10a9cbf7d4e2683ff1d
Johnny Smiles <70188711+Johnny-smiles@users.noreply.github.com>
2020-11-03 11:06:42 -0600

    reworked code

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# Professional README Generator Starter Code
 
-[How to create a Professional README](./readme-guide.md)
+# ReadMe generator. 
+
+## Description
+Users are prompted a series of questions. The answers are then used to auto populate set jquery variables with in the ReadMe mark up language. 
+
+## Indepth
+Sub categories of feature (with descriptions), links, and planned development.
+
+
+## Video Link
+Video link demonstrating assignment: https://drive.google.com/file/d/10BdcbdGivEW0qoRSxO2YSMBjtPPwR-VT/view
--- index.js
+++ index.js
@@ -1,27 +1,13 @@
 const inquirer = require('inquirer')
 const fs = require('fs');
-
+const generateMarkdown = require('./Develop/utils/generateMarkdown')
 
 // array of questions for user
 
 // function to write README file
 const writeFile = answers => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile('.develop/dist/ReadMe.md', answers, err => {
-          // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
-          if (err) {
-            reject(err);
-            // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
-            return;
-          }
-    
-          // if everything went well, resolve the Promise and send the successful data to the `.then()` method
-          resolve({
-            ok: true,
-            message: 'File created!'
-          });
-        });
-      });
+fs.writeFileSync('./develop/dist/ReadMe.md', generateMarkdown(answers))
+
 }
 
 
@@ -29,7 +15,7 @@ const writeFile = answers => {
 
 
 const init = () => {
- return inquirer.prompt([
+  inquirer.prompt([
         {
             type: 'input',
             name: 'name',
@@ -73,12 +59,9 @@ const init = () => {
     .then(answers => {
         console.log("==============")
         console.log(answers)
+        writeFile(answers) 
         console.log("==============")
     });
  };
 
 init()
-.then(answers => {
-    console.log(answers)
-   return writeFile(answers)   
-  })
